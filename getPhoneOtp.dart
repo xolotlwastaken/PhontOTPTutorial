dc83868b9f785e81de6d00fcc08188d0cdac1e5d
just_xolotl <157688992+xolotlwastaken@users.noreply.github.com>
2024-04-27 18:45:38 +0800

    Create getPhoneOtp.dart

---
+++ getPhoneOtp.dart
@@ -0,0 +1,23 @@
+// Automatic FlutterFlow imports
+import '/backend/supabase/supabase.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/actions/index.dart'; // Imports other custom actions
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom action code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+Future getPhoneOtp(String phone) async {
+// Add your function code here!
+
+  // Get a reference your Supabase client
+  final supabase = Supabase.instance.client;
+
+  await supabase.auth.signInWithOtp(
+    phone: phone,
+    shouldCreateUser: false,
+  );
+}
